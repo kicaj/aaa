b5ed47015eba9fed8c29019849a6721f93997006
kicaj <kicaj@kdev.pl>
2018-01-20 14:39:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Deleted plugin for CakePHP
+
+It's still in development mode, do not use in production yet!
+
+## Installation
+
+You can install this plugin into your CakePHP (tested on 3.5) application using [composer](http://getcomposer.org).
+
+The recommended way to install composer packages is:
+
+```
+composer require kicaj/deleted dev-master
+```
---
+++ src/Model/Behavior/DeletedBehavior.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Model\Behavior;
+
+use Cake\Datasource\EntityInterface;
+use Cake\Database\Expression\Comparison;
+use Cake\ORM\Behavior;
+use Cake\ORM\Query;
+use Cake\Event\Event;
+use Cake\I18n\Time;
+
+class DeletedBehavior extends Behavior
+{
+
+    /**
+     * Default config
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'field' => 'deleted'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beforeFind(Event $event, Query $query)
+    {
+        if ($this->_table->hasField($this->_config['field'])) {
+            $deleted = true;
+
+            if ($query->clause('where')) {
+                $query->clause('where')->traverse(function ($expression) use (&$deleted) {
+                    if ($expression instanceof Comparison) {
+                        if ($expression->getField() === $this->_table->getAlias() . '.' . $this->_config['field']) {
+                            $deleted = false;
+                        }
+                    }
+                });
+            }
+
+            if ($deleted === true) {
+                $query->where($this->_table->getAlias() . '.' . $this->_config['field'] . ' IS NULL');
+            }
+        }
+
+        return $query;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beforeDelete(Event $event, EntityInterface $entity)
+    {
+        if ($this->_table->hasField($this->_config['field'])) {
+            $event->stopPropagation();
+
+            $entity = $this->_table->patchEntity($entity, [
+                $this->_config['field'] => Time::now()
+            ]);
+
+            return $this->_table->save($entity);
+        }
+
+        return true;
+    }
+}
